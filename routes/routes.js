7cd2aa7034c538b7ba5e74c6ff49bddeb3455c28
ALDRIN REIS DE MORAIS <40066614+aldrinreis@users.noreply.github.com>
2024-04-21 21:20:48 +0000

    Initial commit

---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+////////////////MÓDULOS /////////////////////
+const express = require("express");
+const router = express.Router();
+//////CONTROLLERS
+const usuarioController = require("../controller/usuarioController");
+////////////////Requisições HTTP Principal /////////////////////
+router.get("/", (req, res) => {
+  return res.json({ message: "Sistema de Varejo" });
+});
+////////////////Requisições HTTP Usuario /////////////////////
+//POST - CADASTRAR
+router.post("/add_usuario", usuarioController.UsuarioCreate);
+//GET - LISTAR
+/*router.get("/usuarios/:id?", usuarioController.UsuarioListar);*/
+//GET - LISTAR
+router.get(
+  "/usuarios/:id?",
+  usuarioController.verificaJWT,
+  usuarioController.UsuarioListar,
+);
+
+//PUT - UPDATE
+router.put("/usuarios/:id", usuarioController.UsuarioUpdate);
+// DELETE
+router.delete("/usuarios/:id", usuarioController.UsuarioDelete);
+
+router.post("/login", usuarioController.UsuarioVerificaLogin);
+
+module.exports = router;
